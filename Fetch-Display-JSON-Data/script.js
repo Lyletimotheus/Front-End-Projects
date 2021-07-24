0a8fb71aaa68721a2c52dc263e39c0af407502f2
Carlos Timotheus <carlostimotheus5@gmail.com>
2021-07-24 21:42:08 +0200

    Fetch API (Final)

--- Fetch-Display-JSON-Data/script.js
+++ Fetch-Display-JSON-Data/script.js
@@ -9,6 +9,7 @@ function printData(dataItem, dataIndex, dataArr) {
   dataItem.forEach((item) => {
     document.querySelector('#output').innerHTML += `
         <h2 style="font-family: 'Segoe UI', Arial, sans-serif"><small>${item.id}</small>. ${item.firstName} ${item.lastName}</h2>
+        <hr>
     `;
   });
 }
